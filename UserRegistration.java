c07563880df553957a7601e47c0de8d9fea73cb1
pranav <prnvkatkar88@gmail.com>
2021-10-26 20:20:02 +0530

    Added:Last name starts with caps and has minimum 3 characters

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,11 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+
+	public boolean firstName(String firstName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
+	}
 	
-	 public boolean firstName(String firstName) {
-	        String regex = "^[A-Z]{1}[a-z]{2,}$";
-	        Pattern pattern = Pattern.compile(regex);
-	        Matcher matcher = pattern.matcher(firstName);
-	        return matcher.matches();
-	    }
+	public boolean lastName(String lastName) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
+	}
 }
--- UserRegistrationJunitTest.java
+++ UserRegistrationJunitTest.java
@@ -7,22 +7,28 @@ import org.junit.jupiter.api.Test;
 import junit.framework.Assert;
 
 class UserRegistrationJunitTest {
+	UserRegistration userRegistration = new UserRegistration();
 
-	@Test
+//	@Test
 	public void givenFirstName_WhenProper_ShouldReturnTrue() {
-		UserRegistration userRegistration = new UserRegistration();
+//		UserRegistration userRegistration = new UserRegistration();
 		boolean result = userRegistration.firstName("Pranav");
 		Assert.assertEquals(true, result);
 
 	}
-	
+
+//	@Test
+	public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
+//		UserRegistration userRegistration = new UserRegistration();
+		boolean result = userRegistration.firstName("pranav");
+		Assert.assertEquals(false, result);
+	}
+
 	@Test
-	 public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
-		UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.firstName("pranav");
-        Assert.assertEquals(false, result);
-    }
-	
-	
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.firstName("Katkar");
+		Assert.assertEquals(true, result);
+
+	}
 
 }
